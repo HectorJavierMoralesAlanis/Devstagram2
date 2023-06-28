63af4cf92fe41abd588e01c2027be3bbebf5c296
HectorJavierMoralesAlanis <124794993+HectorJavierMoralesAlanis@users.noreply.github.com>
2023-06-27 23:52:11 -0600

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -33,18 +33,7 @@
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl text-center font-black my-10">Publicaciones</h2>
-        @if($posts->count())
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-        @foreach($posts as $post)
-            <div>
-                <a href="{{route('posts.show',$string=implode(',',(get_object_vars($post))))}}">
-                    <img src="{{asset('uploads').'/'.$post->imagen}}" alt="Imagen del post {{$post->titulo}}">
-                </a>
-            </div>
-        @endforeach
-        </div>
-        <div>
-            {{$posts->links('pagination::tailwind')}}
+        
         </div>
         @else
             <a class="text-gray-600 uppercase text-center font-bold">No hay publicaciones</a>
